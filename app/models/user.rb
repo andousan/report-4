4ac7da0800650a8e02f4f01d62a4055821083f91
EC2 Default User <ec2-user@ip-172-31-46-172.ec2.internal>
2022-05-10 09:58:05 +0000

    [Add]favorite_comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ApplicationRecord
     validates :name, length: { in: 2..20 }, uniqueness: true
     validates :introduction, length: { maximum: 50 }
          
-  has_many :books, dependent: :destroy
-  
   has_one_attached :profile_image
+  has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   def get_profile_image(width, height)
     unless profile_image.attached?
